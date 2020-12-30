ed30367d80f2af3a3d048affd3daf35dbd7e43b5
Banbridge <banbridge@Banbridge.local>
2020-12-30 14:35:42 +0800

    Initial commit

---
+++ src/main/java/com/bupt/demosystem/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.bupt.demosystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Banbridge on 2020/12/26.
+ */
+@Controller
+public class UserController {
+
+    @RequestMapping("/")
+    public String homePage(){
+        return "home";
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello Word";
+    }
+
+}
---
+++ src/test/java/com/bupt/demosystem/DemosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bupt.demosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
